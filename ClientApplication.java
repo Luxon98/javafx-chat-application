a84ea2e8ace3943ac9f922b920d74715c90e6cc1
Luxon98 <38257808+Luxon98@users.noreply.github.com>
2020-03-21 23:29:25 +0100

    Add files via upload

--- ClientApplication.java
+++ ClientApplication.java
@@ -1,53 +1,52 @@
 package chatclient;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
 
 
 public class ClientApplication {
     private Socket socket;
-    private boolean exitProgramFlag;
     private int userId;
 
     public ClientApplication(String address, int port, int id) {
-        exitProgramFlag = false;
         userId = id;
 
-       // try {
-            //socket = new Socket(address, port);
-            System.out.println("Connected");
-           // DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-           // dataOutputStream.writeInt(userId);
-//        } catch (IOException e) {
-//            System.out.println(e);
-//        }
-
-       // listen();
-       // readInputAndSendMessages();
-    }
-
-    private void readInputAndSendMessages() {
         try {
-            Scanner scanner = new Scanner(System.in);
+            socket = new Socket(address, port);
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            while (true) {
-                try {
-                    String text = scanner.nextLine();
-                    if (text.length() > 0) {
-                        dataOutputStream.writeInt(1);
-                        dataOutputStream.writeUTF(text);
-                    } else {
-                        dataOutputStream.writeInt(-1);
-                        exitProgramFlag = true;
-                        break;
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            dataOutputStream.writeInt(userId);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        listen();
+    }
+
+    public void sendMessage(int id, String message) {
+        if (id > 0) {
+            try {
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                dataOutputStream.writeInt(id);
+                dataOutputStream.writeUTF(message);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendCommand(int command) {
+        if (command < 0) {
+            try {
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeInt(-1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -55,15 +54,20 @@ public class ClientApplication {
         Thread listenThread = new Thread(() -> {
             try {
                 DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-
-                while (!exitProgramFlag) {
+                while (!Client.getInstance().isProgramClosed()) {
                     if (dataInputStream.available() > 0) {
                         int senderId = dataInputStream.readInt();
                         String text = dataInputStream.readUTF();
                         String username = Database.getLogin(senderId);
                         System.out.println(username + ": " + text);
+
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("chat.fxml"));
+                        Parent root = loader.load();
+                        ChatController c = loader.getController();
+                        c.addTextLabel();
                     }
                 }
+                sendCommand(-1);
             } catch (Exception e) {
                 e.printStackTrace();
             }
